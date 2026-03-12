c3b1c6cce52a5212b4633492690fd4a5cb6d302d
V.Varshith <vv9471@srmist.edu.in>
2026-03-12 15:43:22 +0530

    Update UseCasePalindromeCheckerApp.java

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,17 +1,36 @@
-/**
- * ============================================================
- * MAIN CLASS - UseCase1PalindromeCheckerApp
- * ============================================================
- *
- * UC1: Application Entry & Welcome Message
- */
+//uc6
+
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class UseCasePalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String input = "level";
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char ch : input.toCharArray()) {
+            stack.push(ch);
+            queue.add(ch);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.remove())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome");
+        } else {
+            System.out.println("The given string is NOT a Palindrome");
+        }
     }
 }
