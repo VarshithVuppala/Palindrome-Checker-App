3dc6d0578e98a3752bb51e4d9d1ec6598e5a2565
V.Varshith <vv9471@srmist.edu.in>
2026-03-12 15:26:06 +0530

    Update UseCasePalindromeCheckerApp.java

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,17 +1,24 @@
 /**
- * ============================================================
- * MAIN CLASS - UseCase1PalindromeCheckerApp
- * ============================================================
- *
- * UC1: Application Entry & Welcome Message
+ * UC3: Palindrome Check Using String Reverse (Loop)
  */
 
-public class UseCasePalindromeCheckerApp {
+public class UseCase3PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        String input = "level";
+        String reversed = "";
+
+        // Reverse using loop
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        // Compare
+        if (input.equals(reversed)) {
+            System.out.println("The given string is a Palindrome");
+        } else {
+            System.out.println("The given string is NOT a Palindrome");
+        }
     }
 }
